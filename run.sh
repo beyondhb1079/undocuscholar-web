ca1cf4ab83263eaf9076e6ff8f96e66d03dc3a56
Sixto Josue Rios <jrios777@cs.washington.edu>
2016-08-28 02:58:23 +0000

    rename and changed import format

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 # TODO: Detect if being sourced, if not, notify the user
-export FLASK_APP=backend_app.py
+export FLASK_APP=app.py
 export FLASK_DEBUG=1
-# In production: flask run --host=0.0.0.0
+
 PORT=8080
 if [ ! -z "$1" ]; then
     PORT=$1
 fi
+
 flask run --host=0.0.0.0 --port=$PORT
