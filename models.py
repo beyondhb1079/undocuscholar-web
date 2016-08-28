13737cf66d76847ef178c36404de3748a6e38d2f
Sixto Josue Rios <jrios777@cs.washington.edu>
2016-08-28 06:07:18 +0000

    get functionality done, create a scholarship also

--- models.py
+++ models.py
@@ -1,4 +1,6 @@
-from app import db
+from base_app import db
+from flask_sqlalchemy import SQLAlchemy
+import json
 
 class Scholarship(db.Model):
     # TODO: Document max string lengths
@@ -15,7 +17,13 @@ class Scholarship(db.Model):
         self.url = url
 
     def __repr__(self):
-        return '<Name %r, Deadline %r, Amount %r>' % (self.name, self.deadline, self.amount)
+        return '%s - <Name %r, Deadline %r, Amount %r>' % (self.id, self.name, self.deadline, self.amount)
+        
+    def full_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+        
+    def short_dict(self):
+        return {c: getattr(self, c) for c in ['id', 'name', 'deadline', 'amount', 'url']}
 
 # class Subscription(db.Model):
 #     # TODO: Add this for Push Notifications
--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 # TODO: Detect if being sourced, if not, notify the user
 export FLASK_APP=app.py
 export FLASK_DEBUG=1
+export DATABASE_URL=$(heroku config:get DATABASE_URL -a undocuscholar)
 
 PORT=8080
 if [ ! -z "$1" ]; then
