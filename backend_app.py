964a5783e5e164d311d539b816d10fa3848a1b09
Sixto Josue Rios <jrios777@cs.washington.edu>
2016-08-28 02:54:55 +0000

    Setting up Flask-SQLAlchemy to work with Heroku Postgres

--- backend_app.py
+++ backend_app.py
@@ -1,60 +1,21 @@
 import os, sys
-import sqlite3
 import json
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash, Response
 from flask.views import MethodView
+from flask.ext.sqlalchemy import SQLAlchemy
 from werkzeug import Headers
+import psycopg2
+import urlparse
 
 # Create the application
 app = Flask(__name__)
 app.config.from_object(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+db = SQLAlchemy(app)
+
 app.debug = True
-
-# Load default config and override config from an environment variable
-app.config.update(dict(
-    DATABASE=os.path.join(app.root_path, 'backend.db'),
-    SECRET_KEY='development key',
-    USERNAME='admin',
-    PASSWORD='default'
-))
 app.config.from_envvar('API_SETTINGS', silent=True)
-
-def connect_db():
-    """Connects to the specific database."""
-    rv = sqlite3.connect(app.config['DATABASE'])
-    rv.row_factory = sqlite3.Row
-    return rv
-
-def init_db():
-    # TODO: Only init if the db doesn't exist yet
-    db = get_db()
-    with app.open_resource('schema.sql', mode='r') as f:
-        db.cursor().executescript(f.read())
-    db.commit()
-
-@app.cli.command('initdb')
-def initdb_command():
-    """Initializes the database."""
-    init_db()
-    print 'Initialized the database.'
-
-def get_db():
-    """Opens a new database connection if there is none yet for the
-    current application context.
-    """
-    if not hasattr(g, 'sqlite_db'):
-        g.sqlite_db = connect_db()
-    return g.sqlite_db
-
-@app.teardown_appcontext
-def close_db(error):
-    """Closes the database again at the end of the request."""
-    if hasattr(g, 'sqlite_db'):
-        g.sqlite_db.close()
-
-
-# API section
 
 @app.route('/')
 def index():
@@ -117,7 +78,7 @@ class ScholarshipAPI(MethodView):
                 }
               ]
             }
-            response["filters"] = request.args
+            response["request_params"] = request.args
             headers = Headers()
             headers.add('Access-Control-Allow-Origin', '*')
             if "pretty" in request.args:
