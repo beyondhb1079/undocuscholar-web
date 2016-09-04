235a9f32eace914688a2f24636a42ee8227af316
Sixto Josue Rios <jrios777@cs.washington.edu>
2016-09-04 07:29:17 +0000

    Moving into Django. Old files are in old-www/

---
+++ scholarships/models.py
@@ -0,0 +1,8 @@
+from __future__ import unicode_literals
+import datetime
+from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+from django.utils import timezone
+
+class Scholarship(models.Model):
+    pass
