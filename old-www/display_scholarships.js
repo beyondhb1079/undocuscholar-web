a913684c29e55414f84f4559141f9273321b9850
AudreyAwesome <horus1228@yahoo.com>
2016-09-04 22:32:43 -0700

    work so far

--- old-www/display_scholarships.js
+++ old-www/display_scholarships.js
@@ -8,14 +8,16 @@ $(document).ready(function() {
                 
         // Add all the returned scholarships from the JSON 
         //    onto the list of scholarships on the Website
-        $.each(data.results, function(key, value){
+        $.each(data.scholarships, function(key, value){
 
                 var li="<li>";
                 var si="<strong>Scholarship Info:</strong>";
+                var p0="<p>ID:"+ value.id +"</p>";
                 var p1="<p>Name:"+ value.name + "</p>";
                 var p2="<p>deadline:"+ value.deadline + "</p>";
-                var p3="<p>amount:"+ value.amount + "</p></li>";
-                $(".scolarships-list ol").append(li+si+p1+p2+p3);
+                var p3="<p>amount:"+ value.amount + "</p>";
+                var p4="<p>url:"+ value.url+"</p></li>";
+                $(".scolarships-list ol").append(li+si+p0+p1+p2+p3+p4);
 
         });
     });
