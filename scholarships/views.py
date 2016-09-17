612adfa1697aaf6dc028ebb30f5c74d982b1a302
AudreyAwesome <horus1228@yahoo.com>
2016-09-16 21:01:16 -0700

    - Adding Pagination(25 scholarships/pg)

--- scholarships/views.py
+++ scholarships/views.py
@@ -1,16 +1,37 @@
 from django.http import HttpResponse
 from django.template import loader
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.shortcuts import render
 
 from .models import *
 
 
-# Create your views here.
+# # Create your views here.
+# def index(request):
+#     template = loader.get_template('scholarships/index.html')
+#     context = {
+#         'scholarships': Scholarship.objects.all(),
+#     }
+#     return HttpResponse(template.render(context, request))
+
+
+
 def index(request):
-    template = loader.get_template('scholarships/index.html')
-    context = {
-        'scholarships': Scholarship.objects.all(),
-    }
-    return HttpResponse(template.render(context, request))
+    scholarship_list = Scholarship.objects.all()
+    paginator = Paginator(scholarship_list, 25) # Show 25 contacts per page
+
+    page = request.GET.get('page')
+    try:
+        scholarships = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        scholarships = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        scholarships = paginator.page(paginator.num_pages)
+    return render(request, 'scholarships/index.html', {'scholarships': scholarships})
+
+
 
 def detail(request, scholarship_id):
     return HttpResponse("You're looking at scholarship %s." % scholarship_id)
