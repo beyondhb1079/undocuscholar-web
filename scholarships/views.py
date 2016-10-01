c03be6e72a1a36f03fcd64fbc367d7a0337b89aa
Alex Loaiza <dwebcrew@protonmail.ch>
2016-09-30 20:12:44 -0700

    testing new stuff

--- scholarships/views.py
+++ scholarships/views.py
@@ -35,5 +35,17 @@ def collection(request):
         scholarships = paginator.page(paginator.num_pages)
     return render(request, 'scholarships/collection.html', {'scholarships': scholarships})
 
-def detail(scholarship_id):
-    return 'TODO: Page info for scholarship with ID: %s' % scholarship_id
+def sync(reques):
+    scholarship_list = Scholarship.objects.all()
+    paginator = Paginator(scholarship_list, 25)  # Show 25 contacts per page
+
+    page = request.GET.get('page')
+    try:
+        scholarships = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        scholarships = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        scholarships = paginator.page(paginator.num_pages)
+    return render(request, 'scholarships/sync.html', {'scholarships': scholarships})
