5ec328bb20769455376a4d94e83af257836aeb78
Sixto Josue Rios <jrios777@cs.washington.edu>
2017-02-13 14:25:22 -0800

    Fixed requirement and setup issues for linux

--- manage.py
+++ manage.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 from os import environ
 from os.path import exists, getmtime
 
@@ -27,13 +27,13 @@ def setup():
     # Automatically downloads JS/CSS dependencies. It's hackish, but will do for now.
     if last_modified_time('package.json') > last_modified_time('node_modules'):
         # static/node_modules is nonexistent or out of date. Automatically
-        print('Changes in static/package.json detected. Downloading node packages to node_modules...')
+        print('Changes in package.json were detected. Downloading node packages to node_modules...')
         assert which("npm"), "npm needs to be installed: https://nodejs.org/en/download/"
         call([which("npm"), "install"])
 
 if __name__ == "__main__":
-    setup()
     environ.setdefault("DJANGO_SETTINGS_MODULE", "undocuscholar.settings")
+    setup()
     execute_from_command_line(sys.argv)
 
 
