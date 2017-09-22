586c5e09ca0194f942ad6dad4a0d99daa2e64f47
Josue Rios <josuerios777@gmail.com>
2017-09-22 13:10:29 -0700

    Removed some sync() boilerplate and enabled security for non-local environments

--- scholarships/views.py
+++ scholarships/views.py
@@ -36,23 +36,10 @@ def collection(request):
     return render(request, 'scholarships/collection.html', {'scholarships': scholarships})
 
 def sync(request):
-    scholarship_list = Scholarship.objects.all()
-    paginator = Paginator(scholarship_list, 25)  # Show 25 contacts per page
-
-    page = request.GET.get('page')
-    try:
-        scholarships = paginator.page(page)
-    except PageNotAnInteger:
-        # If page is not an integer, deliver first page.
-        scholarships = paginator.page(1)
-    except EmptyPage:
-        # If page is out of range (e.g. 9999), deliver last page of results.
-        scholarships = paginator.page(paginator.num_pages)
-    return render(request, 'scholarships/sync.html', {'scholarships': scholarships})
+    return render(request, 'scholarships/sync.html')
 
 def detail(request, scholarship_id):
     return HttpResponse('TODO: Page info for scholarship with ID: %s' % scholarship_id)
-
 def about(request):
     return render(request, 'scholarships/about.html')
 
