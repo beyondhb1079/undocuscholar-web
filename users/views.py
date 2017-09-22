ac814c3299d86497dcc4598895c2ca238284740e
Josue Rios <josuerios777@gmail.com>
2017-09-22 15:14:14 -0700

    Added rest of users app

---
+++ users/views.py
@@ -0,0 +1,36 @@
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.shortcuts import render, redirect
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'users/register.html', {'form': form})
+
+
+def update(request):
+    if request.method == 'POST':
+        form = UserChangeForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserChangeForm()
+    return render(request, 'users/change.html', {'form': form})
+
+def login(request):
+    return render("TODO")
