248fc32bb836549d473dbe64fe18b20c735f78bf
Josue Rios <josuerios777@gmail.com>
2017-09-22 17:25:02 -0700

    Example Materialize Switch Item

--- users/views.py
+++ users/views.py
@@ -3,6 +3,26 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.shortcuts import render, redirect
 
 
+from django import forms
+
+class SwitchInput(forms.CheckboxInput):
+    template_name = 'switch.html'
+    def __init__(self, on="On", off="Off", attrs=None, check_test=None):
+        super(SwitchInput, self).__init__(attrs, check_test)
+        self.on = on
+        self.off = off
+
+    def get_context(self, name, value, attrs):
+        context = super(SwitchInput, self).get_context(name, value, attrs)
+        context['widget']['on'] = self.on
+        context['widget']['off'] = self.off
+        return context
+
+class NameForm(forms.Form):
+    your_name = forms.CharField(label='Your name', max_length=100, empty_value='guac')
+    your_choice = forms.BooleanField(widget=SwitchInput("Parent", "Student"), initial=True, label="Are you a parent or student?")
+    your_date = forms.DateField(label='Your Date')
+
 def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
@@ -14,7 +34,7 @@ def register(request):
             login(request, user)
             return redirect('home')
     else:
-        form = UserCreationForm()
+        form = NameForm()
     return render(request, 'users/register.html', {'form': form})
 
 
